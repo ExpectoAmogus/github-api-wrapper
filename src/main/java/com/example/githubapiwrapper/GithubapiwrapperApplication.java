27b0c6c0188c41aa3ce3845a0d8974d1b68c8ddb
ExpectoAmogus <72547132+ExpectoAmogus@users.noreply.github.com>
2024-07-23 00:30:15 +0200

    initial commit

---
+++ src/main/java/com/example/githubapiwrapper/GithubapiwrapperApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubapiwrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubapiwrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubapiwrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/githubapiwrapper/GithubapiwrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubapiwrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubapiwrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
